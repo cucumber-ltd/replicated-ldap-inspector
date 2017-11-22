070ed96c5537f6776ff718a9fa68d81f4feaf756
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-11-22 22:31:33 +0000

    Improve output

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 ## Build
 
     export version=15
-    docker build -t registry.replicated.com/cucumberpro/cucumber-ldap-inspector:${version} .
+    docker build -t registry.replicated.com/ldapinspector/replicated-ldap-inspector:${version} .
 
 ## Publish to replicated
 
-    docker push registry.replicated.com/cucumberpro/cucumber-ldap-inspector:${version}
+    docker push registry.replicated.com/ldapinspector/replicated-ldap-inspector:${version}
+
+## Access the hosted version
+
+    http://138.68.181.33:18080/
 
 ## Debugging the Identity API
 
 It can be accessed with cURL on the host:
 
-    curl -k -v -X POST -H "Content-Type: application/json" -d "{"username":"value1", "password":"value2"}" https://138.68.181.33:9880/identity/v1/login
+    curl --insecure --verbose -X POST -H "Content-Type: application/json" -d "{"username":"value1", "password":"value2"}" https://138.68.181.33:9880/identity/v1/login
--- server.js
+++ server.js
@@ -14,24 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/login', (req, res) => {
   authenticate(req.body)
-    .then(apiResponse => res.send(`<!DOCTYPE html>
-<html>
-<body>
-  <h1>Success</h1>
-  <pre>${escapeHtml(JSON.stringify(apiResponse, null, 2))}</pre>
-</body>
-</html>`))
-    .catch(err => res.send(`<!DOCTYPE html>
-<html>
-<body>
-  <h1>Error</h1>
-  <pre>${escapeHtml(err.message)}</pre>
-</body>
-</html>`))
+    .then(response => res.send(html('Response', response)))
+    .catch(err => res.send(html('Error', err.message)))
 })
 
 app.post('/identity/v1/login', (req, res) => res.status(500).send(`Didn't try to log in ${req.body.username} in LDAP.
-REPLICATED_INTEGRATIONAPI is not configured
+REPLICATED_INTEGRATIONAPI is not configured.
 `))
 
 async function authenticate({ username, password }) {
@@ -42,17 +30,42 @@ async function authenticate({ username, password }) {
   }
   const url = `${baseUrl}/identity/v1/login`
   const res = await fetch(url, options)
-
-  if (res.status === 200) {
-    return res.json()
-  }
-  if (res.status === 401) {
-    throw new Error('Authentication failed')
-  }
-  throw new Error(`POST ${url} - ${res.status}\n${await res.text()}`)
+  return curlLikeOutput(res)
 }
 
 app.listen(port)
+
+async function curlLikeOutput(res) {
+  return `< HTTP /1.1 ${res.status} ${res.statusText}
+${headersString(res.headers)}
+${await body(res)}`
+}
+
+function headersString(headers) {
+  let s = ''
+  headers.forEach((value, key) => {
+    s += `< ${key}: ${value}\n`
+  })
+  return s
+}
+
+async function body(res) {
+  if(res.headers.get('Content-Type').indexOf('appliaction/json') === 0) {
+    return JSON.stringify(await res.json(), null, 2)
+  } else {
+    return await res.text()
+  }
+}
+
+function html(header, body) {
+  return `<!DOCTYPE html>
+<html>
+<body>
+<h1>Response</h1>
+<pre>${escapeHtml(body)}</pre>
+</body>
+</html>`
+}
 
 function escapeHtml(unsafe) {
   return unsafe
